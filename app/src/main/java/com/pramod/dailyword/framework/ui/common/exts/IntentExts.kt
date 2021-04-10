13cbec33bd33700a81ed19d7e321c2583db5c851
pramod-sj <pramodsinghjantwal@gmail.com>
2021-04-10 21:24:45 +0530

    Hot fix for app crash happen when sharing app (crashing on device 6, 8 and 9 so far)

--- app/src/main/java/com/pramod/dailyword/framework/ui/common/exts/IntentExts.kt
+++ app/src/main/java/com/pramod/dailyword/framework/ui/common/exts/IntentExts.kt
@@ -2,14 +2,16 @@ package com.pramod.dailyword.framework.ui.common.exts
 
 import android.app.Activity
 import android.app.ActivityOptions
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.paging.ExperimentalPagingApi
+import com.pramod.dailyword.BuildConfig
 import com.pramod.dailyword.R
 import com.pramod.dailyword.framework.ui.aboutapp.AboutAppActivity
 import com.pramod.dailyword.framework.ui.bookmarks.FavoriteWordsActivity
@@ -20,6 +22,8 @@ import com.pramod.dailyword.framework.ui.splash_screen.SplashScreenActivity
 import com.pramod.dailyword.framework.ui.worddetails.WordDetailedActivity
 import com.pramod.dailyword.framework.ui.words.WordListActivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.io.File
+import java.io.FileOutputStream
 
 
 @ExperimentalCoroutinesApi
@@ -42,9 +46,9 @@ fun AppCompatActivity.openHomePage(withFadeAnimation: Boolean = false, finish: B
             android.R.anim.fade_out
         )*/
         val option = ActivityOptions.makeCustomAnimation(
-            this,
-            android.R.anim.fade_in,
-            android.R.anim.fade_out
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
         )
         startActivity(intent, option.toBundle())
     } else {
@@ -57,10 +61,10 @@ fun AppCompatActivity.openHomePage(withFadeAnimation: Boolean = false, finish: B
 
 
 fun Activity.openWordDetailsPage(
-    wordDate: String,
-    option: ActivityOptions? = null,
-    shouldAnimate: Boolean? = false,
-    finish: Boolean = false
+        wordDate: String,
+        option: ActivityOptions? = null,
+        shouldAnimate: Boolean? = false,
+        finish: Boolean = false
 ) {
     val intent = Intent(this, WordDetailedActivity::class.java)
     val bundle = Bundle()
@@ -105,26 +109,38 @@ fun Activity.openAboutPage() {
 }
 
 fun Activity.shareApp(
-    text: String = resources.getString(R.string.share_text) + "\n" + resources.getString(
-        R.string.google_app_url
-    ), bitmap: Bitmap? = null
+        text: String = resources.getString(R.string.share_text) + "\n" + resources.getString(
+                R.string.google_app_url
+        ), bitmap: Bitmap? = null
 ) {
-    val share = Intent(Intent.ACTION_SEND)
-    share.putExtra(Intent.EXTRA_TEXT, text)
-    bitmap?.let {
-        share.type = "image/*"
-        val imageToShare: Uri = Uri.parse(
-            MediaStore.Images.Media.insertImage(
-                contentResolver,
-                bitmap,
-                "Share",
-                null
-            )
-        )
-        share.putExtra(Intent.EXTRA_STREAM, imageToShare)
-    }
-    if (bitmap == null) {
-        share.type = "text/plain";
-    }
-    ContextCompat.startActivity(this, Intent.createChooser(share, "Choose app..."), null)
+
+    val shareWithImage = Intent.createChooser(Intent().apply {
+        action = Intent.ACTION_SEND
+
+        if (bitmap == null) {
+            type = "text/plain";
+            putExtra(Intent.EXTRA_TITLE, "Share Daily Word with your friends and family today!")
+        } else {
+            putExtra(Intent.EXTRA_STREAM, getUriFromBitmap(this@shareApp, bitmap))
+            type = "image/*"
+        }
+
+        putExtra(Intent.EXTRA_TEXT, text)
+
+    }, "Choose app")
+
+    ContextCompat.startActivity(this, shareWithImage, null)
 }
+
+
+fun getUriFromBitmap(context: Context, bitmap: Bitmap): Uri? {
+    return try {
+        val file = File(context.externalCacheDir, "${System.currentTimeMillis()}.jpg")
+        val fos = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos)
+        FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider", file.absoluteFile)
+    } catch (e: Exception) {
+        null
+    }
+}
+
