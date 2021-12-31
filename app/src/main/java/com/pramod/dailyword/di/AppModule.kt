1a87232957c453218d58ad6d2d41fbc973806cbf
pramod-sj <pramodsinghjantwal@gmail.com>
2021-12-31 13:58:17 +0530

    Add tap to search on web feature when search word not found

--- app/src/main/java/com/pramod/dailyword/di/AppModule.kt
+++ app/src/main/java/com/pramod/dailyword/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.pramod.dailyword.di
 
 import android.content.Context
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.judemanutd.autostarter.AutoStartPermissionHelper
 import com.library.audioplayer.AudioPlayer
 import dagger.Module
@@ -26,6 +27,12 @@ object AppModule {
     @Singleton
     fun provideAutoStartPermissionHelper(): AutoStartPermissionHelper {
         return AutoStartPermissionHelper.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAnalytics(@ApplicationContext context: Context): FirebaseAnalytics {
+        return FirebaseAnalytics.getInstance(context)
     }
 
 }
