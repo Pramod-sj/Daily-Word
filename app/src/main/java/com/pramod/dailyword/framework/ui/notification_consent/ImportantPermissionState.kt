521febc34ffe14a8c1b9c61cdb675bfa30e9ff1b
pramod-sj <pramodsinghjantwal@gmail.com>
2024-10-06 18:56:43 +0530

    Add support for disabling unused restriction for app

--- app/src/main/java/com/pramod/dailyword/framework/ui/notification_consent/ImportantPermissionState.kt
+++ app/src/main/java/com/pramod/dailyword/framework/ui/notification_consent/ImportantPermissionState.kt
@@ -1,6 +1,7 @@
 package com.pramod.dailyword.framework.ui.notification_consent
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlarmManager
 import android.app.AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED
 import android.content.BroadcastReceiver
@@ -13,29 +14,33 @@ import android.net.Uri
 import android.os.Build
 import android.os.PowerManager
 import android.provider.Settings
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.IntentCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.content.UnusedAppRestrictionsConstants.API_30
+import androidx.core.content.UnusedAppRestrictionsConstants.API_30_BACKPORT
+import androidx.core.content.UnusedAppRestrictionsConstants.API_31
+import androidx.core.content.UnusedAppRestrictionsConstants.DISABLED
+import androidx.core.content.UnusedAppRestrictionsConstants.ERROR
+import androidx.core.content.UnusedAppRestrictionsConstants.FEATURE_NOT_AVAILABLE
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.common.util.concurrent.ListenableFuture
 import com.pramod.dailyword.framework.helper.scheduleWeeklyAlarmAt12PM
 import com.pramod.dailyword.framework.prefmanagers.PrefManager
 import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import timber.log.Timber
 import javax.inject.Inject
@@ -50,13 +55,19 @@ interface ImportantPermissionHandler {
 
     val isAllowSettingAlarmPermissionGranted: Flow<Boolean>
 
+    val isUnusedAppPausingDisabled: Flow<UnusedAppState>
+
     fun launchNotificationPermissionFlow(): Boolean
 
     fun launchDisableBatteryOptimizationPermissionFlow(): Boolean
 
     fun launchAllowSettingAlarmPermissionFlow(): Boolean
 
+    fun launchDisableUnusedAppPaused(): Boolean
+
 }
+
+enum class UnusedAppState { PAUSING_NOT_AVAILABLE, PAUSING_DISABLED, PAUSING_ENABLED }
 
 @ActivityScoped
 class ActivityImportantPermissionHandler @Inject constructor(
@@ -73,12 +84,16 @@ class ActivityImportantPermissionHandler @Inject constructor(
 
     private val _isAllowSettingAlarmGranted = Channel<Boolean>(1)
 
+    private val _isUnusedAppPausingDisabled = Channel<UnusedAppState>(1)
+
     override val isNotificationPermissionGranted: Flow<Boolean>
         get() = _isNotificationPermissionGranted.receiveAsFlow()
     override val isDisableBatteryOptimizationGranted: Flow<Boolean>
         get() = _isDisableBatteryOptimizationGranted.receiveAsFlow()
     override val isAllowSettingAlarmPermissionGranted: Flow<Boolean>
         get() = _isAllowSettingAlarmGranted.receiveAsFlow()
+    override val isUnusedAppPausingDisabled: Flow<UnusedAppState>
+        get() = _isUnusedAppPausingDisabled.receiveAsFlow()
 
 
     private var isNavigatedToAppDetailPage = false
@@ -158,6 +173,13 @@ class ActivityImportantPermissionHandler @Inject constructor(
         } else false
     }
 
+    override fun launchDisableUnusedAppPaused(): Boolean {
+        isNavigatedToAppDetailPage = true
+        val intent =
+            IntentCompat.createManageUnusedAppRestrictionsIntent(context, context.packageName)
+        return (context as? Activity)?.startActivityForResult(intent, 1010)?.let { true } ?: false
+    }
+
     override fun onCreate(owner: LifecycleOwner) {
         super.onCreate(owner)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -168,13 +190,49 @@ class ActivityImportantPermissionHandler @Inject constructor(
         }
     }
 
+    private var future: ListenableFuture<Int>? = null
+
     override fun onResume(owner: LifecycleOwner) {
         super.onResume(owner)
         if (isNavigatedToAppDetailPage) {
             isNavigatedToAppDetailPage = false
             _isNotificationPermissionGranted.trySend(importantPermissionState.isNotificationEnabled.value)
+            handleUnusedAppRestrictionsStatus()
         }
         importantPermissionState.updatePermissionEnabledState()
+    }
+
+    private fun handleUnusedAppRestrictionsStatus() {
+        future?.cancel(true)
+        future =
+            PackageManagerCompat.getUnusedAppRestrictionsStatus(context)
+        future?.addListener(
+            {
+                when (future?.get()) {
+                    // Status could not be fetched. Check logs for details.
+                    ERROR -> {
+                        _isUnusedAppPausingDisabled.trySend(UnusedAppState.PAUSING_NOT_AVAILABLE)
+                    }
+
+                    // Restrictions do not apply to your app on this device.
+                    FEATURE_NOT_AVAILABLE -> {
+                        _isUnusedAppPausingDisabled.trySend(UnusedAppState.PAUSING_NOT_AVAILABLE)
+                    }
+
+                    // Restrictions have been disabled by the user for your app.
+                    DISABLED -> {
+                        _isUnusedAppPausingDisabled.trySend(UnusedAppState.PAUSING_DISABLED)
+                    }
+
+                    // If the user doesn't start your app for months, its permissions
+                    // will be revoked and/or it will be hibernated.
+                    // See the API_* constants for details.
+                    API_30_BACKPORT, API_30, API_31 -> {
+                        _isUnusedAppPausingDisabled.trySend(UnusedAppState.PAUSING_ENABLED)
+                    }
+                }
+            }, ContextCompat.getMainExecutor(context)
+        )
     }
 
     override fun onDestroy(owner: LifecycleOwner) {
@@ -220,6 +278,11 @@ class ImportantPermissionState @Inject constructor(
         get() = _isSetAlarmEnabled
 
 
+    private val _isUnusedAppPausingDisabled = MutableStateFlow(false)
+    val isUnusedAppPausingDisabled: StateFlow<Boolean>
+        get() = _isUnusedAppPausingDisabled
+
+
     private val _canShowFullNotificationEnableMessage = MutableLiveData(false)
     val canShowFullNotificationEnableMessage: LiveData<Boolean>
         get() = _canShowFullNotificationEnableMessage
@@ -237,6 +300,9 @@ class ImportantPermissionState @Inject constructor(
 
         _canShowFullNotificationEnableMessage.value =
             !isNotificationEnabled.value && !prefManager.isFullNotificationMessageDismissed()
+
+        _isUnusedAppPausingDisabled.value =
+            PackageManagerCompat.getUnusedAppRestrictionsStatus(context).get() == DISABLED
     }
 
     fun markSettingIssueMessageDismissed() {
